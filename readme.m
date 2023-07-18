2c9c0c2d6e6d0e021758dfc5dfd3e9575cb94cc5
kurt krueckeberg <kurt.krueckeberg@gmail.com>
2023-07-18 13:10:39 -0400

    $1

--- readme.m
+++
@@ -1,7 +0,0 @@
-
-- Video howto [download](https://tib.flowcenter.de/mfc/medialink/3/de93b6be1572a5f42f92e4026a0fc5f7acd1ad5fd6e761d2e5d5d7600b26ed4967/ttdasciidocantora.mp4)
-- Antora Documetation on [How to Organize Your Content Files](https://docs.antora.org/antora/latest/organize-content-files/)
-- [Antora 3 Demo Repo](https://github.com/ahus1/antora3demo/tree/main)
- - Ist [slides](https://speakerdeck.com/ahus1/antora-3-release-celebration) click the download icon at bottom
-
-
